27ac06d7d22d224c7721bb95181cb7caddb5c39b
chandu123-123 <103361757+chandu123-123@users.noreply.github.com>
2024-10-11 12:38:37 +0530

    16 added

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['m.media-amazon.com','media.vogue.in','assets.ajio.com','rukminim2.flixcart.com','assets.myntassets.com','static.toiimg.com','karnatakastateopenuniversity.in','www.bewakoof.com','static.iwmbuzz.com'], // Allow images from this domain
+        domains: ['m.media-amazon.com','media.vogue.in','assets.ajio.com','rukminim2.flixcart.com','assets.myntassets.com','static.toiimg.com','i.pinimg.com','www.dnpindiahindi.in','www.jansatta.com','i.pinimg.com','keepmestylish.com','tamilglitz.in','cdn.shopify.com','akm-img-a-in.tosshub.com','karnatakastateopenuniversity.in','www.bewakoof.com','static.iwmbuzz.com','images.meesho.com'], // Allow images from this domain
       },
 };
 
