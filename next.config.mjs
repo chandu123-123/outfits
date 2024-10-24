3c7b46e5b26d3b75b753761950b43a6a0832027d
chandu123-123 <103361757+chandu123-123@users.noreply.github.com>
2024-10-24 17:22:47 +0530

    pooja 50 complete

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['m.media-amazon.com','media.vogue.in','assets.ajio.com','rukminim2.flixcart.com','assets.myntassets.com','static.toiimg.com','cdn.gulte.com','www.gethucinema.com','content.tupaki.com','cdn.gulte.com','i.pinimg.com','www.dnpindiahindi.in','www.jansatta.com','i.pinimg.com','keepmestylish.com','tamilglitz.in','cdn.shopify.com','www.gethucinema.com','w0.peakpx.com','pbs.twimg.com','pakkatelugu.com','encrypted-tbn0.gstatic.com','m.sakshipost.com','akm-img-a-in.tosshub.com','assets.vogue.in','karnatakastateopenuniversity.in','pakkatelugu.com','www.bewakoof.com','static.iwmbuzz.com','images.meesho.com','www.atozpictures.com','www.bollywoodhungama.com','blogger.googleusercontent.com'], // Allow images from this domain
+        domains: ['m.media-amazon.com','media.vogue.in','assets.ajio.com','rukminim2.flixcart.com','assets.myntassets.com','static.toiimg.com','cdn.gulte.com','www.gethucinema.com','content.tupaki.com','cdn.gulte.com','i.pinimg.com','www.dnpindiahindi.in','www.jansatta.com','i.pinimg.com','keepmestylish.com','tamilglitz.in','cdn.shopify.com','www.gethucinema.com','w0.peakpx.com','pbs.twimg.com','pakkatelugu.com','encrypted-tbn0.gstatic.com','m.sakshipost.com','akm-img-a-in.tosshub.com','assets.vogue.in','karnatakastateopenuniversity.in','pakkatelugu.com','www.bewakoof.com','static.iwmbuzz.com','images.meesho.com','www.atozpictures.com','www.bollywoodhungama.com','blogger.googleusercontent.com','wallpapercave.com','cdn.siasat.com'], // Allow images from this domain
       },
 };
 
